de4f12f27f89e63cf63c5c43cf7a465709ee50b9
Ben Mazur <ben.mazur@gmail.com>
2013-12-14 15:44:30 -0500

    food conducts alpha
    Only detecting when we click on a food item, not whether it was actually
    used

--- ConductEventListener.java
+++ ConductEventListener.java
@@ -1,5 +1,8 @@
 package conducts;
 
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -18,7 +21,22 @@ public class ConductEventListener {
 	{
 		System.out.println("Player interacting!");
 		if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) {
-			
+			if (event.entityPlayer.getCurrentEquippedItem() != null && event.entityPlayer.getCurrentEquippedItem().getItemUseAction() == EnumAction.eat) {
+				System.out.println("Player interacting with a FOOD ! " + event.entityPlayer.getCurrentEquippedItem());
+				
+				event.entityPlayer.addStat(ConductsPage.breatharian, 1);
+				
+				ItemFood currentFood = event.entityPlayer.getCurrentEquippedItem().getItem() instanceof ItemFood ? (ItemFood)(event.entityPlayer.getCurrentEquippedItem().getItem()) : null;
+				
+				if (currentFood != null) {
+					if (currentFood.isWolfsFavoriteMeat()) {
+						event.entityPlayer.addStat(ConductsPage.vegan, 1);
+						event.entityPlayer.addStat(ConductsPage.vegetarian, 1);
+					} else {
+						event.entityPlayer.addStat(ConductsPage.carnivore, 1);
+					}
+				}
+			}
 		}
 	}
 	
--- ConductsPage.java
+++ ConductsPage.java
@@ -9,10 +9,15 @@ import net.minecraftforge.common.AchievementPage;
 public class ConductsPage extends AchievementPage {
 	
 	public static Achievement breatharian = new Achievement(6000000, "breatharian", 0, -2, Block.glass, (Achievement)null).setIndependent().setSpecial().registerAchievement();
-	public static Achievement carnivore = new Achievement(6000001, "carnivore", -1, 0, Item.book, breatharian).registerAchievement();
+	public static Achievement carnivore = new Achievement(6000001, "carnivore", -1, 0, Item.beefRaw, breatharian).registerAchievement();
 	public static Achievement vegan = new Achievement(6000002, "vegan", 1, 0, Item.carrot, breatharian).registerAchievement();
-	public static Achievement vegetarian = new Achievement(6000003, "vegetarian", 1, 2, Item.carrot, vegan).registerAchievement();
+	public static Achievement vegetarian = new Achievement(6000003, "vegetarian", 1, 2, Item.cake, vegan).registerAchievement();
 	
+	
+	public static Achievement pacifist = new Achievement(6000004, "pacifist", 4, 0, Block.plantRed, (Achievement)null).setIndependent().setSpecial().registerAchievement();
+	public static Achievement neverSword = new Achievement(6000005, "neverSword", 3, 0, Item.swordIron, pacifist).registerAchievement();
+	public static Achievement neverRange = new Achievement(6000006, "neverRange", 5, 0, Item.arrow, pacifist).registerAchievement();
+
 	
 	
 
@@ -25,8 +30,8 @@ public class ConductsPage extends AchievementPage {
         LanguageRegistry.instance().addStringLocalization("achievement.carnivore", "en_US", "Carnivore");
         LanguageRegistry.instance().addStringLocalization("achievement.carnivore.desc", "en_US", "Never eat a vegetable!");
 
-        LanguageRegistry.instance().addStringLocalization("achievement.carnivore", "en_US", "Vegan");
-        LanguageRegistry.instance().addStringLocalization("achievement.carnivore.desc", "en_US", "Do not eat milk or eggs");
+        LanguageRegistry.instance().addStringLocalization("achievement.vegan", "en_US", "Vegan");
+        LanguageRegistry.instance().addStringLocalization("achievement.vegan.desc", "en_US", "Do not eat milk or eggs");
 
         LanguageRegistry.instance().addStringLocalization("achievement.vegetarian", "en_US", "Vegetarian");
         LanguageRegistry.instance().addStringLocalization("achievement.vegetarian.desc", "en_US", "Never eat a meat!");

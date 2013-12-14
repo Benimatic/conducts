518965f3d0351ced46ba2f313c9cf9af39d28680
Ben Mazur <ben.mazur@gmail.com>
2013-12-14 17:00:47 -0500

    hooked up pacifist conducts

--- ConductEventListener.java
+++ ConductEventListener.java
@@ -1,9 +1,17 @@
 package conducts;
 
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
 import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerDestroyItemEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
@@ -19,28 +27,76 @@ public class ConductEventListener {
 	@ForgeSubscribe
 	public void interact(PlayerInteractEvent event)
 	{
-		System.out.println("Player interacting!");
+//		System.out.println("Player interacting!");
 		if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) {
 			if (event.entityPlayer.getCurrentEquippedItem() != null && event.entityPlayer.getCurrentEquippedItem().getItemUseAction() == EnumAction.eat) {
-				System.out.println("Player interacting with a FOOD ! " + event.entityPlayer.getCurrentEquippedItem());
+//				System.out.println("Player interacting with a FOOD ! " + event.entityPlayer.getCurrentEquippedItem() + " player is " + event.entityPlayer);
 				
-				event.entityPlayer.addStat(ConductsPage.breatharian, 1);
+				event.entityPlayer.triggerAchievement(ConductsPage.breatharian);
 				
 				ItemFood currentFood = event.entityPlayer.getCurrentEquippedItem().getItem() instanceof ItemFood ? (ItemFood)(event.entityPlayer.getCurrentEquippedItem().getItem()) : null;
 				
 				if (currentFood != null) {
 					if (currentFood.isWolfsFavoriteMeat()) {
-						event.entityPlayer.addStat(ConductsPage.vegan, 1);
-						event.entityPlayer.addStat(ConductsPage.vegetarian, 1);
+						event.entityPlayer.triggerAchievement(ConductsPage.vegan);
+						event.entityPlayer.triggerAchievement(ConductsPage.vegetarian);
 					} else {
-						event.entityPlayer.addStat(ConductsPage.carnivore, 1);
+						event.entityPlayer.triggerAchievement(ConductsPage.carnivore);
 					}
 				}
 			}
 		}
 	}
 	
+	@ForgeSubscribe
+	public void entityHurts(LivingHurtEvent event)
+	{
+		if (event.source.getSourceOfDamage() instanceof EntityPlayer)
+		{
+			// discover the cause of the damage
+			EntityPlayer cause = (EntityPlayer)(event.source.getSourceOfDamage());
+
+			cause.triggerAchievement(ConductsPage.pacifist);
+			
+			// check what the player is holding
+			if (cause.getCurrentEquippedItem() != null) {
+				Item itemUsed = cause.getCurrentEquippedItem().getItem();
+				
+				if (itemUsed instanceof ItemSword || itemUsed instanceof ItemTool) {
+					cause.triggerAchievement(ConductsPage.neverSword);
+				}
+			}
+			
+			if (event.source.isProjectile())
+			{
+				cause.triggerAchievement(ConductsPage.neverRange);
+			}
+			
+			
+		}
+	}
 	
 	
-	
+	@ForgeSubscribe
+	public void entityDies(LivingDeathEvent event)
+	{
+		if (event.source.getSourceOfDamage() instanceof EntityPlayer)
+		{
+			// discover the cause of the death
+			EntityPlayer cause = (EntityPlayer)(event.source.getSourceOfDamage());
+			
+			if (event.entityLiving instanceof EntityAnimal)
+			{
+				cause.triggerAchievement(ConductsPage.noKillAnimal);
+			}	
+			if (event.entityLiving instanceof IMob)
+			{
+				cause.triggerAchievement(ConductsPage.noKillMob);
+			}
+			if (event.entityLiving instanceof EntityPlayer)
+			{
+				cause.triggerAchievement(ConductsPage.noKillPlayers);
+			}
+		}
+	}
 }
--- ConductsPage.java
+++ ConductsPage.java
@@ -8,21 +8,24 @@ import net.minecraftforge.common.AchievementPage;
 
 public class ConductsPage extends AchievementPage {
 	
-	public static Achievement breatharian = new Achievement(6000000, "breatharian", 0, -2, Block.glass, (Achievement)null).setIndependent().setSpecial().registerAchievement();
+	public static Achievement breatharian = new Achievement(6000000, "breatharian", 0, -2, Block.glass, (Achievement)null).setSpecial().registerAchievement();
 	public static Achievement carnivore = new Achievement(6000001, "carnivore", -1, 0, Item.beefRaw, breatharian).registerAchievement();
 	public static Achievement vegan = new Achievement(6000002, "vegan", 1, 0, Item.carrot, breatharian).registerAchievement();
 	public static Achievement vegetarian = new Achievement(6000003, "vegetarian", 1, 2, Item.cake, vegan).registerAchievement();
 	
 	
-	public static Achievement pacifist = new Achievement(6000004, "pacifist", 4, -1, Block.plantRed, (Achievement)null).setIndependent().setSpecial().registerAchievement();
-	public static Achievement neverSword = new Achievement(6000005, "neverSword", 3, 0, Item.swordIron, pacifist).registerAchievement();
-	public static Achievement neverRange = new Achievement(6000006, "neverRange", 5, 0, Item.arrow, pacifist).registerAchievement();
+	public static Achievement pacifist = new Achievement(6000004, "pacifist", 4, -2, Block.plantRed, (Achievement)null).setSpecial().registerAchievement();
+	public static Achievement neverSword = new Achievement(6000005, "neverSword", 3, 0, Item.arrow, pacifist).registerAchievement();
+	public static Achievement neverRange = new Achievement(6000006, "neverRange", 5, 0, Item.swordGold, pacifist).registerAchievement();
+	public static Achievement noKillMob = new Achievement(6000007, "noKillMob", 3, -3, Item.skull, pacifist).registerAchievement();
+	public static Achievement noKillAnimal = new Achievement(6000008, "noKillPassive", 4, -4, Item.leather, pacifist).registerAchievement();
+	public static Achievement noKillPlayers = new Achievement(6000009, "noKillPlayers", 5, -3, Item.plateGold, pacifist).registerAchievement();
 
 	
 	
 
 	public ConductsPage() {
-		super(ConductsMod.NAME, breatharian, carnivore, vegan, vegetarian, pacifist, neverSword, neverRange);
+		super(ConductsMod.NAME, breatharian, carnivore, vegan, vegetarian, pacifist, neverSword, neverRange, noKillMob, noKillAnimal, noKillPlayers);
 		
         LanguageRegistry.instance().addStringLocalization("achievement.breatharian", "en_US", "Breatharian");
         LanguageRegistry.instance().addStringLocalization("achievement.breatharian.desc", "en_US", "Never eat.");
@@ -31,7 +34,7 @@ public class ConductsPage extends AchievementPage {
         LanguageRegistry.instance().addStringLocalization("achievement.carnivore.desc", "en_US", "Never eat a vegetable!");
 
         LanguageRegistry.instance().addStringLocalization("achievement.vegan", "en_US", "Vegan");
-        LanguageRegistry.instance().addStringLocalization("achievement.vegan.desc", "en_US", "Do not eat milk or eggs");
+        LanguageRegistry.instance().addStringLocalization("achievement.vegan.desc", "en_US", "Do not eat meat, milk, or eggs");
 
         LanguageRegistry.instance().addStringLocalization("achievement.vegetarian", "en_US", "Vegetarian");
         LanguageRegistry.instance().addStringLocalization("achievement.vegetarian.desc", "en_US", "Never eat a meat!");
@@ -44,6 +47,15 @@ public class ConductsPage extends AchievementPage {
 
         LanguageRegistry.instance().addStringLocalization("achievement.neverRange", "en_US", "Way of the Sword");
         LanguageRegistry.instance().addStringLocalization("achievement.neverRange.desc", "en_US", "Never strike at range");
+
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillMob", "en_US", "Save the Monsters!");
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillMob.desc", "en_US", "Never kill a monster");
+
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillPassive", "en_US", "Save the Animals!");
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillPassive.desc", "en_US", "Never kill an animal");
+
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillPlayers", "en_US", "Save the Players");
+        LanguageRegistry.instance().addStringLocalization("achievement.noKillPlayers.desc", "en_US", "Never kill another player");
 
 	}
 
